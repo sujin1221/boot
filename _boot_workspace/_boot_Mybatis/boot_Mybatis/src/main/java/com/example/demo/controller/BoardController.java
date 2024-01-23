278221c509ca213275d84804a23b4c3617f8142d
sujin1221 <143565345+sujin1221@users.noreply.github.com>
2024-01-23 15:14:36 +0900

    Add files via upload

---
+++ _boot_workspace/_boot_Mybatis/boot_Mybatis/src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/board/*")
+@Controller
+public class BoardController {
+	
+	private final BoardService bsv;
+	
+	@GetMapping("/register")
+	public void register() {}
+	
+	
+}
