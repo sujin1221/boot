8604492169ca4385de2bdaf02314f0c44e513fe5
sujin1221 <143565345+sujin1221@users.noreply.github.com>
2024-01-24 14:54:59 +0900

    Add files via upload

--- _boot_workspace/_boot_Mybatis/boot_Mybatis/src/main/java/com/example/demo/repogitory/BoardMapper.java
+++ _boot_workspace/_boot_Mybatis/boot_Mybatis/src/main/java/com/example/demo/repogitory/BoardMapper.java
@@ -1,8 +1,24 @@
 package com.example.demo.repogitory;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.domain.BoardVO;
 
 @Mapper
 public interface BoardMapper {
+
+	int insert(BoardVO bvo);
+
+	List<BoardVO> selectList();
+
+	int updateModify(BoardVO bvo);
+
+	void readCount(int bno);
+
+	BoardVO selectDetail(int bno);
+
+	int delete(int bno);
 	
 }
--- _boot_workspace/_boot_Mybatis/boot_Mybatis/src/main/java/com/example/demo/service/BoardServiceImpl.java
+++ _boot_workspace/_boot_Mybatis/boot_Mybatis/src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.BoardVO;
 import com.example.demo.repogitory.BoardMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -12,4 +15,31 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class BoardServiceImpl implements BoardService{
 	private final BoardMapper mapper;
+
+	@Override
+	public int register(BoardVO bvo) {
+		log.info("register service impl");
+		int isOk = mapper.insert(bvo);
+		return isOk;
+	}
+
+	@Override
+	public List<BoardVO> getList() {		
+		return mapper.selectList();
+	}
+
+	@Override
+	public int modify(BoardVO bvo) {		
+		return mapper.updateModify(bvo);		
+	}
+
+	@Override
+	public BoardVO getDetail(int bno) {		
+		return mapper.selectDetail(bno);
+	}
+
+	@Override
+	public int delete(int bno) {		
+		return mapper.delete(bno);
+	}
 }
